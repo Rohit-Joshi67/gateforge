74dfeae73db91d0223e45ec275f94f92ca32c545
rohit <workwithrj07@gmail.com>
2026-07-14 13:04:34 +0530

    Milestone 1: Initial Spring Boot skeleton for GateForge

---
+++ gateforge/src/main/java/com/gateforge/GateforgeApplication.java
@@ -0,0 +1,13 @@
+package com.gateforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateforgeApplication.class, args);
+	}
+
+}
---
+++ gateforge/src/test/java/com/gateforge/GateforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
